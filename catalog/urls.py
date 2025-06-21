ef079b393bb3b80ca6e0885273fb1a79038b3cd2
dj3747 <dj94911131@gmail.com>
2025-06-21 22:59:27 +0300

    create README.md

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
-from . import views
 
+from catalog.apps import CatalogConfig
+
+from . import views
 
 app_name = CatalogConfig.name
 urlpatterns = [path("", views.home, name="home"),
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
@@ -9,7 +9,6 @@ def home(request):
 def contacts(request):
     if request.method == "POST":
         name = request.POST.get("name")
-        message = request.POST.get("message")
 
         return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
     return render(request, "catalog/contacts.html")
