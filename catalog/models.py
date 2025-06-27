b9563e6b439a85b76a70620456b27a8910f6e42c
dj3747 <dj94911131@gmail.com>
2025-06-28 00:46:55 +0300

    create superuser

--- catalog/models.py
+++ catalog/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, verbose_name="Описание")
     image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name="Изображение")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products", verbose_name="Категория")
-    purchase_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
 
