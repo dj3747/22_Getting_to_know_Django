efe76be8918b38ecb978342b6719553a8f2bc8d9
dj3747 <dj94911131@gmail.com>
2025-07-25 15:36:01 +0300

    create app users

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-from django.conf.global_settings import STATICFILES_DIRS, MEDIA_URL, MEDIA_ROOT
+from django.conf.global_settings import STATICFILES_DIRS, MEDIA_URL, MEDIA_ROOT, AUTH_USER_MODEL
 
 load_dotenv(override=True)
 
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "catalog",
     "blog",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -109,3 +110,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Медиа файлы загружаемые пользователем
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+AUTH_USER_MODEL = "users.CustomUser"
